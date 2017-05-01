65b1a10d95d5eaa5410ac0c8b493c8980cc5932d
TEN_TACLES\Admin <neumannj@fh-trier.de>
2017-05-01 08:12:53 +0200

    Erste Logik für die Felder und das gesamte Spielfeld, sowie für das generieren des selben.

---
+++ PlantUP/Assets/WaterTile.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTile : MonoBehaviour, isTile
+{
+    PlayingFieldLogic playingField;
+    isTile[] neighbours;
+
+    int windstrength;
+    bool windUpdate = true;
+
+
+
+
+    /// <summary>
+    /// Die Energiemenge die das Wasser hergibt.
+    /// </summary>
+    int waterStrength;
+
+    public static int minimumWaterStrength = 50;
+    public static int maximumWaterStrength = 150;
+    
+
+    public void forceWindUpdate()
+    {
+        windUpdate = true;
+    }
+
+    public int getLightValue()
+    {
+        return playingField.getLightStrength();
+    }
+
+    public isTile[] getNeighbours()
+    {
+        return neighbours;
+    }
+
+    public string getTileType()
+    {
+        return "water";
+    }
+
+    
+    public int getWindSpread()
+    {
+        //WasserFelder verändern die Windgeschwindigkeit nicht.
+        return getWindStrength();
+    }
+
+    public int getWindStrength()
+    {
+        if (windUpdate)
+            updateWindStrength();
+        return windstrength;
+    }
+
+   
+    public void updateWindStrength()
+    {
+        if (windUpdate)
+        {
+            //Findet das Feld aus dem der Wind auf dieses Feld weht.
+            isTile tile = neighbours[playingField.getWindDirection()];
+            if (tile != null)
+            {
+                windstrength = tile.getWindSpread();
+            }
+            else
+            {
+                windstrength = playingField.getWindStrength();
+            }
+            //Die Windstärke wurde geupdated.
+            windUpdate = false;
+        }
+    }
+
+
+    public void setNeighbours(isTile[] neighbours)
+    {
+        this.neighbours = neighbours;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void setWaterStrength(int waterStrength)
+    {
+        this.waterStrength = waterStrength;
+    }
+
+    public int getWaterStrength()
+    {
+        return waterStrength;
+    }
+
+
+    public void setPlayingField(PlayingFieldLogic playingField)
+    {
+        this.playingField = playingField;
+    }
+
+    public PlayingFieldLogic getPlayingField()
+    {
+        return playingField;
+    }
+}
