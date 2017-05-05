022277436bc1f217a023e110c06c24a3fada1d6f
Jacques Urbany <urbanyj@hochschule-tirer.de>
2017-05-05 20:39:24 +0200

    Adding Plant and BLueprint Scripts.

---
+++ PlantUP/Assets/_Scripts/Plant/Blueprint.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blueprint : MonoBehaviour {
+
+    /// <summary>
+    /// Upgrade zum Erhöhen der FoV und des Alters.
+    /// </summary>
+    int height = 1;
+    int maxHeight = 6;
+    /// <summary>
+    /// Upgrade zum Erhöhen der hps, erhöht aber auch eps.
+    /// </summary>
+    int regeneration = 0;
+    int maxRegeneration = 10;
+    /// <summary>
+    /// Upgrade zur Aufnahme von Energie über benachbarte WaterTiles.
+    /// </summary>
+    int deepRoots = 0;
+    int maxDeepRoots = 10;
+    /// <summary>
+    /// Upgrade zur Aufnahme von Energie über Windstärke.
+    /// </summary>
+    int bigLeaves = 0;
+    int maxBigLeaves = 10;
+    /// <summary>
+    /// Upgrade zur Aufnahme von Energie über Sonne.
+    /// </summary>
+    int largPetals = 0;
+    int maxLargePetals = 10;
+    /// <summary>
+    /// Upgrade zur Aufnahme von Energie in der GroundTile.
+    /// </summary>
+    int porousRoots = 0;
+    int maxPorousRoots = 10;
+    /// <summary>
+    /// Upgrade zur Aufnahme von Energie über benachbarte GroundTiles.
+    /// </summary>
+    int spreadRoots = 0;
+    int maxSpreadRoots = 5;
+    /// <summary>
+    /// Upgrade zum Schutz vor Angriffen, hemmt Energieaufnahme.
+    /// </summary>
+    int thickStalk = 0;
+    int maxThickStalk = 5;
+    /// <summary>
+    /// Upgrade zur effizienteren Energieausgabe.
+    /// </summary>
+    int efficiency = 0;
+    int maxEfficieny = 5;
+    /// <summary>
+    /// Upgrade zur Chance auf periodische Samenverteilung.
+    /// </summary>
+    int insects = 0;
+    int maxInsects = 3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PlantUP/Assets/_Scripts/Plant/Plant.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour {
+
+    /// <summary>
+    /// Eine Referenz auf die GroundTile.
+    /// </summary>
+    GroundTile myTile;
+
+    /// <summary>
+    /// Eine Referenz auf den BluePrint.
+    /// </summary>
+    Blueprint myBlueprint;
+
+    /// <summary>
+    /// Das Alter der Pflanze.
+    /// </summary>
+    int age;
+
+    /// <summary>
+    /// Das maximale Alter der Pflanze.
+    /// </summary>
+    int maxAge;
+
+    /// <summary>
+    /// Die Sichtweite der Pflanze in Tiles.
+    /// </summary>
+    int fieldOfView;
+
+    /// <summary>
+    /// Die Lebenskraft der Pflanze.
+    /// </summary>
+    int health;
+
+    /// <summary>
+    /// Health Per Second. Wieviel Leben regeneriert die Pflanze.
+    /// </summary>
+    float hps;
+
+    /// <summary>
+    /// Die Nährstoffabsorbier Fähigkeit der Pflanze. 
+    /// </summary>
+    float nutrientAbsorb;
+
+    /// <summary>
+    /// Die Grösse des Energiespeichers der Pflanze.
+    /// </summary>
+    int bankCapacity;
+
+    /// <summary>
+    /// Der Inhalt des Energiespeichers der Pflanze.
+    /// </summary>
+    int bankLevel;
+
+    /// <summary>
+    /// Wieviel Schaden die Pflanze nimmt in Prozent.
+    /// </summary>
+    float damageTaken;
+
+    /// <summary>
+    /// Energy Per Second. Wieviel Energie die Pflanze verbraucht.
+    /// </summary>
+    float eps;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public int getHealth() { return health; }
+    public int getAge() { return age; }
+    public int getMaxAge() { return maxAge; }
+    public int getFoV() { return fieldOfView; }
+    public float getRegen() { return hps; }
+    public float getNutrientAbsorb() { return nutrientAbsorb; }
+    public int getBankCapacity() { return bankCapacity; }
+    public int getBankLevel() { return bankLevel; }
+    public float getDamageTaken() { return damageTaken;  }
+    public float getEnergyOut() { return eps; }
+
+
+    public void setTile(GroundTile myTile)
+    {
+        this.myTile = myTile;
+    }
+
+    public GroundTile getTile()
+    {
+        return myTile;
+    }
+
+    public void setBlueprint(Blueprint myBlueprint)
+    {
+        this.myBlueprint = myBlueprint;
+    }
+
+    public Blueprint getBlueprint()
+    {
+        return myBlueprint;
+    }
+}
