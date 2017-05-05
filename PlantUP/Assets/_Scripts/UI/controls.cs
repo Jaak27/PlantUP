13d4f3087a52e9d921bf88e37f298c87b8f8d07f
draegesd <toaster101@outlook.de>
2017-05-05 12:35:30 +0200

    UI
    erste UI Elemente um Feldinfos an zu zeigen und Fähigkeiten zu
    "benutzen"

---
+++ PlantUP/Assets/_Scripts/UI/controls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controls : MonoBehaviour {
+
+    private Transform cameraControl; // Transform Component der Camera
+
+
+    // Use this for initialization
+    void Start () {
+
+        cameraControl = GetComponent<Transform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Dient zur Bewegung der Camera mit den Pfeiltasten
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            cameraControl.Translate(new Vector3(0.25f, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            cameraControl.Translate(new Vector3(-0.25f, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            cameraControl.Translate(new Vector3(0, -0.25f, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            cameraControl.Translate(new Vector3(0, 0.25f, 0));
+        }
+        
+
+    }
+}
