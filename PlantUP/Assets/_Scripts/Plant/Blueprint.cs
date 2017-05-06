8184049172cabb34cda8db986fb81e6e851e98d4
Jacques Urbany <urbanyj@hochschule-tirer.de>
2017-05-06 20:07:56 +0200

    Changes to Plant and Blueprint scripts.

--- PlantUP/Assets/_Scripts/Plant/Blueprint.cs
+++ PlantUP/Assets/_Scripts/Plant/Blueprint.cs
@@ -7,13 +7,13 @@ public class Blueprint : MonoBehaviour {
     /// <summary>
     /// Upgrade zum Erhöhen der FoV und des Alters.
     /// </summary>
-    int height = 1;
+    int height = 0;
     int maxHeight = 6;
     /// <summary>
     /// Upgrade zum Erhöhen der hps, erhöht aber auch eps.
     /// </summary>
     int regeneration = 0;
-    int maxRegeneration = 10;
+    int maxRegeneration = 6;
     /// <summary>
     /// Upgrade zur Aufnahme von Energie über benachbarte WaterTiles.
     /// </summary>
@@ -27,8 +27,8 @@ public class Blueprint : MonoBehaviour {
     /// <summary>
     /// Upgrade zur Aufnahme von Energie über Sonne.
     /// </summary>
-    int largPetals = 0;
-    int maxLargePetals = 10;
+    int largePetals = 0;
+    int maxLargePetals = 5;
     /// <summary>
     /// Upgrade zur Aufnahme von Energie in der GroundTile.
     /// </summary>
@@ -56,12 +56,92 @@ public class Blueprint : MonoBehaviour {
     int maxInsects = 3;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public int getHeight() { return height; }
+    public int getRegeneration() { return regeneration; }
+    public int getDeepRoots() { return deepRoots; }
+    public int getBigLeaves() { return bigLeaves; }
+    public int getLargePetals() { return largePetals; }
+    public int getPorousRoots() { return porousRoots; }
+    public int getSpreadRoots() { return spreadRoots; }
+    public int getThickStalk() { return thickStalk; }
+    public int getEfficiency() { return efficiency; }
+    public int getInsects() { return insects; }
+
+    public void setHeight(int i) {
+        if (i <= maxHeight || i >= 0) {
+            height = i;
+        }
+    }
+    public void setRegeneration(int i)
+    {
+        if (i <= maxRegeneration || i >= 0)
+        {
+            regeneration = i;
+        }
+    }
+    public void setDeepRoots(int i)
+    {
+        if (i <= maxDeepRoots || i >= 0)
+        {
+            deepRoots = i;
+        }
+    }
+    public void setBigLeaves(int i)
+    {
+        if (i <= maxBigLeaves || i >= 0)
+        {
+            bigLeaves = i;
+        }
+    }
+    public void setLargePetals(int i)
+    {
+        if (i <= maxLargePetals || i >= 0)
+        {
+            largePetals = i;
+        }
+    }
+    public void setPorousRoots(int i)
+    {
+        if (i <= maxPorousRoots || i >= 0)
+        {
+            porousRoots = i;
+        }
+    }
+    public void setSpreadRoots(int i)
+    {
+        if (i <= maxSpreadRoots || i >= 0)
+        {
+            spreadRoots = i;
+        }
+    }
+    public void setThickStalk(int i)
+    {
+        if (i <= maxThickStalk || i >= 0)
+        {
+            thickStalk = i;
+        }
+    }
+    public void setEfficiency(int i)
+    {
+        if (i <= maxEfficieny || i >= 0)
+        {
+            efficiency = i;
+        }
+    }
+    public void setInsects(int i)
+    {
+        if (i <= maxInsects || i >= 0)
+        {
+            insects = i;
+        }
+    }
 }
--- PlantUP/Assets/_Scripts/Plant/Plant.cs
+++ PlantUP/Assets/_Scripts/Plant/Plant.cs
@@ -17,22 +17,21 @@ public class Plant : MonoBehaviour {
     /// <summary>
     /// Das Alter der Pflanze.
     /// </summary>
-    int age;
-
-    /// <summary>
-    /// Das maximale Alter der Pflanze.
-    /// </summary>
+    int age = 0;
     int maxAge;
 
     /// <summary>
     /// Die Sichtweite der Pflanze in Tiles.
     /// </summary>
+    int baseFov = 2;
     int fieldOfView;
 
     /// <summary>
     /// Die Lebenskraft der Pflanze.
     /// </summary>
+    int maxHealth;
     int health;
+    int baseHealth = 100;
 
     /// <summary>
     /// Health Per Second. Wieviel Leben regeneriert die Pflanze.
@@ -43,11 +42,13 @@ public class Plant : MonoBehaviour {
     /// Die Nährstoffabsorbier Fähigkeit der Pflanze. 
     /// </summary>
     float nutrientAbsorb;
+    float baseNutrientAbsorb = 1f;
 
     /// <summary>
     /// Die Grösse des Energiespeichers der Pflanze.
     /// </summary>
     int bankCapacity;
+    int baseBankCapacity = 1000;
 
     /// <summary>
     /// Der Inhalt des Energiespeichers der Pflanze.
@@ -58,6 +59,7 @@ public class Plant : MonoBehaviour {
     /// Wieviel Schaden die Pflanze nimmt in Prozent.
     /// </summary>
     float damageTaken;
+    float baseDamageTaken = 1f;
 
     /// <summary>
     /// Energy Per Second. Wieviel Energie die Pflanze verbraucht.
@@ -78,12 +80,12 @@ public class Plant : MonoBehaviour {
     public int getAge() { return age; }
     public int getMaxAge() { return maxAge; }
     public int getFoV() { return fieldOfView; }
-    public float getRegen() { return hps; }
+    public float getHps() { return hps; }
     public float getNutrientAbsorb() { return nutrientAbsorb; }
     public int getBankCapacity() { return bankCapacity; }
     public int getBankLevel() { return bankLevel; }
     public float getDamageTaken() { return damageTaken;  }
-    public float getEnergyOut() { return eps; }
+    public float getEps() { return eps; }
 
 
     public void setTile(GroundTile myTile)
@@ -104,5 +106,64 @@ public class Plant : MonoBehaviour {
     public Blueprint getBlueprint()
     {
         return myBlueprint;
+    }
+
+    public void pushChanges() {
+
+    }
+    /// <summary>
+    /// MaxHealth abhängig von height.
+    /// </summary>
+    private void pushMaxHealth() {
+        int height = myBlueprint.getHeight();
+        maxHealth = (int)System.Math.Pow(2, height) * baseHealth;
+    }
+    /// <summary>
+    /// Field of View abhängig von height
+    /// </summary>
+    private void pushFoV() {
+        int height = myBlueprint.getHeight();
+        fieldOfView = baseFov + height;
+    }
+    //TODO: Define params
+    private void pushMaxAge() {
+
+    }
+    /// <summary>
+    /// DamageTaken abhängig von efficiency und thickStalk, range 50% - 150%
+    /// </summary>
+    private void pushDamageTaken() {
+        int efficiency = myBlueprint.getEfficiency();
+        int thickStalk = myBlueprint.getThickStalk();
+
+        damageTaken = baseDamageTaken + (thickStalk * 0.1f) - (efficiency * 0.1f);
+    }
+    /// <summary>
+    /// HPS von regeneration und insect abhängig,
+    /// bei negativ spielt damageTaken eine Rolle
+    /// </summary>
+    private void pushHps() {
+        int regen = myBlueprint.getRegeneration();
+        int insects = myBlueprint.getInsects();
+
+        hps = (maxHealth * regen * 0.02f) - (maxHealth * insects * 0.01f * damageTaken);
+    }
+    /// <summary>
+    /// Basic Nutrient Absorb Value abhängig von thickStalk und largePetals, range 50% - 150%
+    /// </summary>
+    private void pushNutrientAbsorb() {
+        int thickStalk = myBlueprint.getThickStalk();
+        int largePetals = myBlueprint.getLargePetals();
+
+        nutrientAbsorb = baseNutrientAbsorb + (largePetals * 0.1f) - (thickStalk * 0.1f);
+    }
+    /// <summary>
+    /// Maximale Bank Kapazität abhängig von height und thickStalk
+    /// </summary>
+    private void pushBankCapacity() {
+        int thickStalk = myBlueprint.getThickStalk();
+        int height = myBlueprint.getHeight();
+
+        bankCapacity = baseBankCapacity + (height * 100) + (thickStalk * 25);
     }
 }
